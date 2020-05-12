4b639394fb81e0e15a46198a6da0ffbe982795a2
John DeRegnaucourt <jdereg@gmail.com>
2020-05-12 08:58:51 -0400

    naming things better

--- src/main/groovy/com/cedarsoftware/util/MathUtil.java
+++ src/main/groovy/com/cedarsoftware/util/MathUtil.java
@@ -1,9 +1,29 @@
 package com.cedarsoftware.util;
 
+/**
+ * This class exists because using divide operator on long's in Groovy is slow and returns a BigDecimal.
+ * Groovy has 25L.intdiv(5L), but that does extra intermediate steps.
+ * 
+ * @author John DeRegnaucourt (jdereg@gmail.com)
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License")
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
 public class MathUtil
 {
-    public static long divide(long x, long y)
+    public static long divide(long dividend, long divisor)
     {
-        return x / y;
+        return dividend / divisor;
     }
 }
